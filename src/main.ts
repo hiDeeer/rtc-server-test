c5978eab8a6f3836cea58424564b498a04bd237b
김의현(toma_to) <tomato1756@dgsw.hs.kr>
2024-10-01 20:24:01 +0900

    CORS main.ts 에도 추가

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // CORS 활성화
+
+  app.enableCors({
+    origin: 'https://225c-221-168-22-204.ngrok-free.app', // 허용할 출처
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 메소드
+    credentials: true, // 인증 정보 포함 여부
+  });
+
   await app.listen(3002);
 }
 bootstrap();
